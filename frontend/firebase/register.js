02f2c5de65b63e42354e357aae5cc7d33583d3e1
arindam <ariyoucandoit@gmail.com>
2023-08-19 17:59:33 +0530

    firebase collection issue

--- frontend/firebase/register.js
+++ frontend/firebase/register.js
@@ -1,6 +1,6 @@
 import {auth,db } from "../config/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { collection } from "firebase/firestore";
+import { collection,setDoc,doc } from "firebase/firestore";
 
 function emailRegister({ email, password }) {
   console.log(email);
@@ -9,35 +9,31 @@ function emailRegister({ email, password }) {
 }
 function registerDatabase({ id, email, name, surname }) {
   try {
-    return collection(db,"Users").doc(id).set({
-      name,
-      surname,
-      email,
-      addresses: [],
-      cart: {},
-      favorites: [],
-      orders: [],
-      phoneNumber: "",
-      photoUrl: null,
-    });
+    // return collection(db,"Users").doc(id).set({
+    //   name,
+    //   surname,
+    //   email,
+    //   addresses: [],
+    //   cart: {},
+    //   favorites: [],
+    //   orders: [],
+    //   phoneNumber: "",
+    //   photoUrl: null,
+    // });
+    return setDoc(doc(collection(db,"Users")),{
+        name,
+        surname,
+        email,
+        addresses: [],
+        cart: {},
+        favorites: [],
+        orders: [],
+        phoneNumber: "",
+        photoUrl: null,
+      });
   } catch (e) {
     console.error(e);
     return null;
   }
 }
-
-function registerSellerDatabase({id, email, name}) {
-  return collection(db,"Users").doc(id).set({
-    name,
-    email,
-    products: [],
-    addresses: [],
-    cart: {},
-    favorites: [],
-    orders: [],
-    phoneNumber: "",
-    photoUrl: null,
-  });
-}
-
-export { emailRegister, registerDatabase,registerSellerDatabase };
+export { emailRegister, registerDatabase};
--- frontend/firebase/update-user.js
+++ frontend/firebase/update-user.js
@@ -1,24 +1,33 @@
-import { firebase, auth, db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
+import { storage } from "../config/firebase";
+import { updateDoc,collection,doc,getDoc } from "firebase/firestore";
 
 function updateUser({ email, name, surname, phoneNumber, photo, finalEvent }) {
   const currentUser = auth.currentUser.uid;
 
   if (photo) {
-    return firebase
-      .storage()
+    return storage()
       .ref("images/" + currentUser + (photo?.name || "0"))
       .put(photo)
       .then((doc) => {
         doc.ref.getDownloadURL().then((url) => {
-          db.collection("Users")
-            .doc(currentUser)
-            .update({
-              name,
-              surname,
-              email,
-              phoneNumber: phoneNumber || "",
-              photoUrl: url,
-            })
+          // collection(db,"Users")
+          //   .doc(currentUser)
+          //   .update({
+          //     name,
+          //     surname,
+          //     email,
+          //     phoneNumber: phoneNumber || "",
+          //     photoUrl: url,
+          //   })
+          updateDoc(doc(collection(db,"Users")),currentUser,{
+                name,
+                surname,
+                email,
+                phoneNumber: phoneNumber || "",
+                photoUrl: url,
+              }
+          )
             .catch((e) => console.log(e))
             .finally(() => finalEvent());
         });
